550b4f21a8b1a882ee950508ded43ec03782017f
ClementPla <clement.playout@polymtl.ca>
2026-01-30 11:46:19 -0500

    Try to fix MacOS mouse event

--- src/app/Components/pages/editor/drawable-canvas/directives/canvas-input.directive.ts
+++ src/app/Components/pages/editor/drawable-canvas/directives/canvas-input.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+import { Directive, EventEmitter, Output, ElementRef, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { ZoomPanService } from '../service/zoom-pan.service';
 import { EditorService } from '../../services/editor.service';
 import { Point2D } from '../interface';
@@ -8,41 +8,69 @@ import { DrawService } from '../service/draw.service';
   selector: '[appCanvasInput]',
   standalone: true
 })
-export class CanvasInputDirective {
+export class CanvasInputDirective implements OnInit, OnDestroy {
   @Output() canvasMove = new EventEmitter<{event: MouseEvent, coords: Point2D, cursor: Point2D}>();
   @Output() canvasUp = new EventEmitter<MouseEvent>();
 
+  private element: HTMLElement;
+
   constructor(
+    private elementRef: ElementRef,
     private zoomPanService: ZoomPanService,
     private editorService: EditorService,
-    private drawService: DrawService
-  ) {}
+    private drawService: DrawService,
+    private ngZone: NgZone
+  ) {
+    this.element = this.elementRef.nativeElement;
+  }
 
-  @HostListener('mousedown', ['$event'])
-  @HostListener('touchstart', ['$event'])
-  onMouseDown(event: MouseEvent | TouchEvent) {
+  ngOnInit() {
+    // Run outside Angular zone for better performance
+    this.ngZone.runOutsideAngular(() => {
+      // Mouse events
+      this.element.addEventListener('mousedown', this.onMouseDown.bind(this));
+      this.element.addEventListener('mousemove', this.onMouseMove.bind(this));
+      this.element.addEventListener('mouseup', this.onMouseUp.bind(this));
+      this.element.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+      
+      // Touch events with passive: false for Safari
+      this.element.addEventListener('touchstart', this.onMouseDown.bind(this), { passive: false });
+      this.element.addEventListener('touchmove', this.onMouseMove.bind(this), { passive: false });
+      this.element.addEventListener('touchend', this.onMouseUp.bind(this), { passive: false });
+    });
+  }
 
+  ngOnDestroy() {
+    this.element.removeEventListener('mousedown', this.onMouseDown.bind(this));
+    this.element.removeEventListener('mousemove', this.onMouseMove.bind(this));
+    this.element.removeEventListener('mouseup', this.onMouseUp.bind(this));
+    this.element.removeEventListener('mouseleave', this.onMouseLeave.bind(this));
+    this.element.removeEventListener('touchstart', this.onMouseDown.bind(this));
+    this.element.removeEventListener('touchmove', this.onMouseMove.bind(this));
+    this.element.removeEventListener('touchend', this.onMouseUp.bind(this));
+  }
+
+  private onMouseDown = (event: MouseEvent | TouchEvent) => {
     const mouseEvent = this.normalizeEvent(event);
     
-    // Middle click or space+click logic
     if (mouseEvent.button === 1) {
       this.editorService.activatePanMode();
     }
     
-     if (this.editorService.canPan()) {
+    if (this.editorService.canPan()) {
       this.zoomPanService.startDrag(mouseEvent);
     } else {
       this.drawService.startDraw(mouseEvent);
     }
-  }
+  };
 
-  @HostListener('mousemove', ['$event'])
-  @HostListener('touchmove', ['$event'])
-  onMouseMove(event: MouseEvent | TouchEvent) {
-    if (event instanceof TouchEvent) event.preventDefault();
-    const mouseEvent = this.normalizeEvent(event);
+  private onMouseMove = (event: MouseEvent | TouchEvent) => {
+    if (event instanceof TouchEvent) {
+      event.preventDefault();
+    }
     
-    const target = mouseEvent.target as HTMLElement;
+    const mouseEvent = this.normalizeEvent(event);
+    const target = this.element;
     const rect = target.getBoundingClientRect();
     
     const coords = this.zoomPanService.getImageCoordinates(mouseEvent);
@@ -50,34 +78,45 @@ export class CanvasInputDirective {
       x: mouseEvent.clientX - rect.left,
       y: mouseEvent.clientY - rect.top
     };
-
     
+    // Run inside Angular zone for change detection
+    this.ngZone.run(() => {
+      this.canvasMove.emit({ event: mouseEvent, coords, cursor });
+    });
+  };
 
-    this.canvasMove.emit({ event: mouseEvent, coords, cursor });
-  }
-
-  @HostListener('mouseup', ['$event'])
-  @HostListener('touchend', ['$event'])
-  async onMouseUp(event: MouseEvent | TouchEvent) {
+  private onMouseUp = async (event: MouseEvent | TouchEvent) => {
     const mouseEvent = this.normalizeEvent(event);
+    
     if (mouseEvent.button === 1) {
       this.editorService.restoreLastTool();
     }
-     this.zoomPanService.endDrag();
+    
+    this.zoomPanService.endDrag();
+    
     if (!this.editorService.canPan()) {
       await this.drawService.endDraw(mouseEvent);
-
     }
-  }
+  };
 
-  @HostListener('mouseleave', ['$event'])
-  async onMouseLeave(event: MouseEvent | TouchEvent) {
-    this.onMouseUp(event);
-  }
+  private onMouseLeave = async (event: MouseEvent | TouchEvent) => {
+    await this.onMouseUp(event);
+  };
 
   private normalizeEvent(event: MouseEvent | TouchEvent): MouseEvent {
     if (event instanceof MouseEvent) return event;
+    
     const touch = event.touches[0] || event.changedTouches[0];
+    
+    if (!touch) {
+      console.warn('No touch point found');
+      return new MouseEvent('normalized', {
+        clientX: 0,
+        clientY: 0,
+        button: 0,
+      });
+    }
+    
     return new MouseEvent('normalized', {
       clientX: touch.clientX,
       clientY: touch.clientY,
