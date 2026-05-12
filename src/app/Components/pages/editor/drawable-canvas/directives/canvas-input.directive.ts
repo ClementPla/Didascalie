28c94a97638f9ce23478826accdbc2bea492db4f
ClementPla <clement.playout@polymtl.ca>
2026-05-12 19:12:01 -0400

    Fix TouchEvent bug

--- src/app/Components/pages/editor/drawable-canvas/directives/canvas-input.directive.ts
+++ src/app/Components/pages/editor/drawable-canvas/directives/canvas-input.directive.ts
@@ -6,27 +6,31 @@ import { DrawService } from '../service/draw.service';
 
 @Directive({
   selector: '[appCanvasInput]',
-  standalone: true
+  standalone: true,
 })
 export class CanvasInputDirective {
-  @Output() canvasMove = new EventEmitter<{event: MouseEvent, coords: Point2D, cursor: Point2D}>();
+  @Output() canvasMove = new EventEmitter<{
+    event: MouseEvent;
+    coords: Point2D;
+    cursor: Point2D;
+  }>();
   @Output() canvasUp = new EventEmitter<MouseEvent>();
 
   constructor(
     private zoomPanService: ZoomPanService,
     private editorService: EditorService,
-    private drawService: DrawService
+    private drawService: DrawService,
   ) {}
 
   @HostListener('mousedown', ['$event'])
   @HostListener('touchstart', ['$event'])
   onMouseDown(event: MouseEvent | TouchEvent) {
     const mouseEvent = this.normalizeEvent(event);
-    
+
     if (mouseEvent.button === 1) {
       this.editorService.activatePanMode();
     }
-    
+
     if (this.editorService.canPan()) {
       this.zoomPanService.startDrag(mouseEvent);
     } else {
@@ -40,17 +44,17 @@ export class CanvasInputDirective {
     if (this.isTouchEvent(event)) {
       event.preventDefault();
     }
-    
+
     const mouseEvent = this.normalizeEvent(event);
     const target = mouseEvent.target as HTMLElement;
     const rect = target.getBoundingClientRect();
-    
+
     const coords = this.zoomPanService.getImageCoordinates(mouseEvent);
     const cursor = {
       x: mouseEvent.clientX - rect.left,
-      y: mouseEvent.clientY - rect.top
+      y: mouseEvent.clientY - rect.top,
     };
-    
+
     this.canvasMove.emit({ event: mouseEvent, coords, cursor });
   }
 
@@ -58,13 +62,13 @@ export class CanvasInputDirective {
   @HostListener('touchend', ['$event'])
   async onMouseUp(event: MouseEvent | TouchEvent) {
     const mouseEvent = this.normalizeEvent(event);
-    
+
     if (mouseEvent.button === 1) {
       this.editorService.restoreLastTool();
     }
-    
+
     this.zoomPanService.endDrag();
-    
+
     if (!this.editorService.canPan()) {
       await this.drawService.endDraw(mouseEvent);
     }
@@ -83,24 +87,20 @@ export class CanvasInputDirective {
   }
 
   private normalizeEvent(event: MouseEvent | TouchEvent): MouseEvent {
-    if (!this.isTouchEvent(event)) {
-      return event as MouseEvent;
-    }
-    
-    const touch = event.touches[0] || event.changedTouches[0];
-    
-    if (!touch) {
-      return new MouseEvent('normalized', {
-        clientX: 0,
-        clientY: 0,
-        button: 0,
-      });
-    }
-    
-    return new MouseEvent('normalized', {
+    if (!this.isTouchEvent(event)) return event;
+
+    const touch = event.changedTouches[0] || event.touches[0];
+    if (!touch) return null as any; // or change return type to MouseEvent | null
+
+    const synthetic = new MouseEvent('normalized', {
       clientX: touch.clientX,
       clientY: touch.clientY,
       button: 0,
     });
+    Object.defineProperty(synthetic, 'target', { value: event.target });
+    Object.defineProperty(synthetic, 'currentTarget', {
+      value: event.currentTarget,
+    });
+    return synthetic;
   }
 }
